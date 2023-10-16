bea512d223a0b0d67b3a184571d56ed977dee1a2
wayyoung <wayyoung@gmail.com>
2023-10-16 21:33:35 +0800

    Create CustomFirebaseFS.h

---
+++ CustomFirebaseFS.h
@@ -0,0 +1,16 @@
+#ifndef CUSTOMER_FB_H_
+#define CUSTOMER_FB_H
+// DISABLE_NTP_TIME // For disabling the NTP time setting
+// DISABLE_ERROR_STRING // For disabling the error string from error reason
+DISABLE_RTDB // For disabling RTDB support
+DISABLE_ERROR_QUEUE // For disabling RTDB Error Queue support
+DISABLE_FCM // For disabling Firebase Cloud Messaging support
+DISABLE_PSRAM // For disabling PSRAM support
+//DISABLE_OTA // For disabling OTA updates support
+//DISABLE_KEEP_ALIVE // For disabling TCP Keep Alive support (See TCP Keep Alive)
+DISABLE_SD // For disabling flash filesystem support
+DISABLE_FLASH // For disabling SD filesystem support
+DISABLE_DEBUG // For disable debug port
+
+// FIREBASE_DISABLE_ALL_OPTIONS // For disabling all predefined build options above
+#endif
